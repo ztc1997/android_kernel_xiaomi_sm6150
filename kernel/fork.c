201bf1013b08223cfa51c04532779b13aef9f970
ztc1997 <dev@ztc1997.com>
2021-03-16 23:15:33 +0800

    devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting
    This will enable us to more accurately replicate Pixel 4 userspace
    DDR boosting by adding support for both CPUBW and LLCCBW devices.
    
    https://android.googlesource.com/device/google/coral/+/refs/heads/master/init.power.rc
    https://android.googlesource.com/device/google/coral/+/refs/heads/master/powerhint.json
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2209,9 +2209,11 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current))
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+	/* Boost DDR bus to the max for 150 ms when userspace launches an app */
+	if (task_is_zygote(current)){
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 150);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 150);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
