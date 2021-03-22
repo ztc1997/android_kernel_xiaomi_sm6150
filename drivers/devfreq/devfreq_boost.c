38c19dd35929f66ccb1c3c88ab639318fd6b0f9b
ztc1997 <dev@ztc1997.com>
2021-03-22 16:59:33 +0800

    drm/drm_atomic: Boost only if last interaction was within 3s
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -13,6 +13,8 @@
 #include <linux/slab.h>
 #include <uapi/linux/sched/types.h>
 
+unsigned long last_input_time;
+
 enum {
 	SCREEN_OFF,
 	INPUT_BOOST,
@@ -233,6 +235,8 @@ static void devfreq_boost_input_event(struct input_handle *handle,
 
 	for (i = 0; i < DEVFREQ_MAX; i++)
 		__devfreq_boost_kick(&d->devices[i]);
+
+	last_input_time = jiffies;
 }
 
 static int devfreq_boost_input_connect(struct input_handler *handler,
