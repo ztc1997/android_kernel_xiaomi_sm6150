e3212592c12debe6295978bad033911d3843b2c9
ztc1997 <dev@ztc1997.com>
2021-04-06 18:31:40 +0800

    Add script to automatically package

---
+++ build.sh
@@ -0,0 +1,50 @@
+ANYKERNEL3_DIR=$PWD/AnyKernel3
+FINAL_KERNEL_ZIP=kernel-phoenix-r-ztc1997-$(cat version)-$(git rev-parse --short=7 HEAD).zip
+IMAGE_GZ=$PWD/out/arch/arm64/boot/Image.gz
+DTB=$PWD/out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb
+DTBO_IMG=$PWD/out/arch/arm64/boot/dtbo.img
+
+make -j$(nproc --all) O=out \
+                      ARCH="arm64" \
+                      CC="clang" \
+                      CROSS_COMPILE="aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+                      AR="llvm-ar" \
+                      NM="llvm-nm" \
+                      OBJCOPY="llvm-objcopy" \
+                      OBJDUMP="llvm-objdump" \
+                      STRIP="llvm-strip" \
+                      2>&1 | tee kernel.log
+
+echo "**** Verify target files ****"
+if [ ! -f "$IMAGE_GZ" ]; then
+    echo "!!! Image.gz not found"
+    exit 1
+fi
+if [ ! -f "$DTB" ]; then
+    echo "!!! dtb not found"
+    exit 1
+fi
+if [ ! -f "$DTBO_IMG" ]; then
+    echo "!!! dtbo.img not found"
+    exit 1
+fi
+
+echo "**** Moving target files ****"
+mv $IMAGE_GZ $ANYKERNEL3_DIR/Image.gz
+mv $DTB $ANYKERNEL3_DIR/dtb
+mv $DTBO_IMG $ANYKERNEL3_DIR/dtbo.img
+
+echo "**** Time to zip up! ****"
+cd $ANYKERNEL3_DIR/
+zip -r9 $FINAL_KERNEL_ZIP *
+
+echo "**** Removing leftovers ****"
+cd ..
+rm $ANYKERNEL3_DIR/Image.gz
+rm $ANYKERNEL3_DIR/dtb
+rm $ANYKERNEL3_DIR/dtbo.img
+
+mv -f $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP out/
+
+echo "Check out/$FINAL_KERNEL_ZIP"
---
+++ env.sh
@@ -0,0 +1,5 @@
+export ARCH=arm64
+export SUBARCH=arm64
+export DTC_EXT=$PWD/../tools/dtc
+export PATH=$PWD/../tools/proton-clang/bin/:$PATH
+export LD_LIBRARY_PATH=$PWD/../tools/proton-clang/lib/:$LD_LIBRARY_PATH
