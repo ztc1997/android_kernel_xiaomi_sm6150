5194604cbdfb8ca8e128ec46511baf05ac8c15af
ztc1997 <dev@ztc1997.com>
2021-04-08 10:37:07 +0800

    kernel: Use task_is_zygote to fix build

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -556,7 +556,7 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	/* This covers boosting for app launches and app transitions */
 	if (!ret && !threadgroup &&
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
-		is_zygote_pid(task->parent->pid)) {
+		task_is_zygote(task->parent)) {
 		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 150);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 150);
 	}
