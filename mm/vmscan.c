c6fd9e3d7ffefe41350a83e3ab34ee2dbbe5c203
ztc1997 <dev@ztc1997.com>
2021-04-12 14:00:14 +0800

    Revert "mm: vmpressure: Ignore allocation orders above PAGE_ALLOC_COSTLY_ORDER"
    This reverts commit a2e815cd5a6a19b5fcee2ba5746a5a80a0872060.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2718,7 +2718,7 @@ static bool shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 			/* Record the group's reclaim efficiency */
 			vmpressure(sc->gfp_mask, memcg, false,
 				   sc->nr_scanned - scanned,
-				   sc->nr_reclaimed - reclaimed, sc->order);
+				   sc->nr_reclaimed - reclaimed);
 
 			/*
 			 * Direct reclaim and kswapd have to scan all memory
@@ -2757,11 +2757,6 @@ static bool shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 			sc->nr_reclaimed += reclaim_state->reclaimed_slab;
 			reclaim_state->reclaimed_slab = 0;
 		}
-
-		/* Record the subtree's reclaim efficiency */
-		vmpressure(sc->gfp_mask, sc->target_mem_cgroup, true,
-			   sc->nr_scanned - nr_scanned,
-			   sc->nr_reclaimed - nr_reclaimed, sc->order);
 
 		if (sc->nr_reclaimed - nr_reclaimed)
 			reclaimable = true;
