ca8ff71eb3cf199cdde90bd41859563d4306d57a
ztc1997 <dev@ztc1997.com>
2021-04-25 13:15:13 +0800

    lpm-levels: Don't expose sleep_disabled
    * Don't allow userspace do anything with it

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -109,12 +109,6 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 static bool print_parsed_dt;
 module_param_named(print_parsed_dt, print_parsed_dt, bool, 0664);
 
-static bool sleep_disabled;
-module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
-
-static bool sleep_disabled_touch;
-module_param_named(sleep_disabled_touch, sleep_disabled_touch, bool, 0664);
-
 /**
  * msm_cpuidle_get_deep_idle_latency - Get deep idle latency value
  *
@@ -138,7 +132,6 @@ uint32_t register_system_pm_ops(struct system_pm_ops *pm_ops)
 
 void lpm_disable_for_input(bool on)
 {
-	sleep_disabled_touch = !!on;
 	return;
 }
 EXPORT_SYMBOL(lpm_disable_for_input);
@@ -650,9 +643,6 @@ static int cpu_power_select(struct cpuidle_device *dev,
 	uint32_t min_residency, max_residency;
 	struct power_params *pwr_params;
 	uint64_t bias_time = 0;
-
-	if (((sleep_disabled || sleep_disabled_touch) && !cpu_isolated(dev->cpu)) || sleep_us < 0)
-		return best_level;
 
 	idx_restrict = cpu->nlevels + 1;
 
