a0675e0eeb6c4bd91d5a95281c7185bc185c56ef
ztc1997 <dev@ztc1997.com>
2021-05-06 18:43:20 +0800

    ion: Run freeing buffers on LITTLE CPU cluster

--- drivers/staging/android/ion/ion_heap.c
+++ drivers/staging/android/ion/ion_heap.c
@@ -255,8 +255,8 @@ int ion_heap_init_deferred_free(struct ion_heap *heap)
 #endif
 	INIT_LIST_HEAD(&heap->free_list);
 	init_waitqueue_head(&heap->waitqueue);
-	heap->task = kthread_run(ion_heap_deferred_free, heap,
-				 "%s", heap->name);
+	heap->task = kthread_run_perf_critical(cpu_lp_mask, 
+				 ion_heap_deferred_free, heap, "%s", heap->name);
 	if (IS_ERR(heap->task)) {
 		pr_err("%s: creating thread for deferred free failed\n",
 		       __func__);
