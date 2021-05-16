1fb390c4d207a56ba8a59ab636e86d59e92e6a06
ztc1997 <dev@ztc1997.com>
2021-05-16 23:09:42 +0800

    Revert "clk: qcom: gpucc-sdmmagpie: undervolt"
    This reverts commit 5049742191694119abeb8889e8fbedfeeb2ebf1c.

--- drivers/clk/qcom/vdd-level-sdmmagpie.h
+++ drivers/clk/qcom/vdd-level-sdmmagpie.h
@@ -19,7 +19,6 @@
 
 enum vdd_levels {
 	VDD_NONE,
-	VDD_RETENTION,		/* RETENTION */
 	VDD_MIN,		/* MIN SVS */
 	VDD_LOWER,		/* SVS2 */
 	VDD_LOW,		/* SVS */
@@ -31,7 +30,6 @@ enum vdd_levels {
 
 static int vdd_corner[] = {
 	RPMH_REGULATOR_LEVEL_OFF,		/* VDD_NONE */
-	RPMH_REGULATOR_LEVEL_RETENTION,		/* VDD_RETENTION */
 	RPMH_REGULATOR_LEVEL_MIN_SVS,		/* VDD_MIN */
 	RPMH_REGULATOR_LEVEL_LOW_SVS,		/* VDD_LOWER */
 	RPMH_REGULATOR_LEVEL_SVS,		/* VDD_LOW */
